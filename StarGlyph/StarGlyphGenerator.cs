6c84bb31b26f40fb69b71e8a245eebb7d1f72c26
RocketPrinter <35203190+RocketPrinter@users.noreply.github.com>
2021-11-02 17:14:33 +0200

    better input

--- StarGlyph/StarGlyphGenerator.cs
+++ StarGlyph/StarGlyphGenerator.cs
@@ -25,7 +25,7 @@ public static class StarGlyphGenerator
         '0','1','2','3','4','5','6','7','8','9',
         ' ','+','-','*','/','='
     };
-    public static readonly char[] sentenceSeparators = new[] { '.', '?', '!' };
+    public static readonly char[] sentenceSeparators = new[] { '.', '?', '!', '\n' };
 
     public static SvgDocument CharToSVG(char c, StarGlyphOptions? options = null)
     {
